39ae16a6d9882104a15fa17d50416b64dc505a4e
xio810 <boi4806@naver.com>
2021-10-27 20:49:54 +0900

    짝수와 홀수

--- src/App.java
+++ src/App.java
@@ -3,21 +3,23 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int num = sc.nextInt();
 
-        int answer = 0;
-
-        if (a > b) {
-            int tmp = a;
-            a = b;
-            b = tmp;
+        // v1
+        if (num % 2 == 0) {
+            System.out.println("EVEN");
+        } else {
+            System.out.println("ODD");
         }
 
-        for (int i = a; i <= b; i++) {
-            answer += i;
+        // v2
+        if (num % 2 == 1) {
+            System.out.println("ODD");
+        } else {
+            System.out.println("EVEN");
         }
 
-        System.out.println(answer);
+        // v3 (프로그래머스 return때)
+        // num % 2 == 0 ? "Even" : "Odd";
     }
 }
