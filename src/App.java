3e240e69da698c288ee54a31925c9a01cc9caa39
xio810 <boi4806@naver.com>
2021-10-28 19:53:29 +0900

    x만큼 간격이 있는 n개의 숫자

--- src/App.java
+++ src/App.java
@@ -3,23 +3,19 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+        int x = sc.nextInt();
+        int n = sc.nextInt();
 
-        // v1
-        if (num % 2 == 0) {
-            System.out.println("EVEN");
-        } else {
-            System.out.println("ODD");
+        int[] arr = { 2, 4, 5, 6, 2, 33 };
+
+        System.out.println("'======'");
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i] = x * (i + 1));
+            // arr[0] = 4 * (0 + 1) => 4
+            // arr[1] = 4 * (1 + 1) => 8
+            // arr[2] = 4 * (2 + 1) => 12
         }
 
-        // v2
-        if (num % 2 == 1) {
-            System.out.println("ODD");
-        } else {
-            System.out.println("EVEN");
-        }
-
-        // v3 (프로그래머스 return때)
-        // num % 2 == 0 ? "Even" : "Odd";
     }
 }
