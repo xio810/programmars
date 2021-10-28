0d482e532d31141f092f99f3e73988d8fe62fab9
xio810 <boi4806@naver.com>
2021-10-28 21:07:35 +0900

    약수의합

--- src/App.java
+++ src/App.java
@@ -3,19 +3,16 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
         int n = sc.nextInt();
 
-        int[] arr = { 2, 4, 5, 6, 2, 33 };
+        int answer = 0;
 
-        System.out.println("'======'");
-
-        for (int i = 0; i < n; i++) {
-            System.out.println(arr[i] = x * (i + 1));
-            // arr[0] = 4 * (0 + 1) => 4
-            // arr[1] = 4 * (1 + 1) => 8
-            // arr[2] = 4 * (2 + 1) => 12
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer += i;
+            }
         }
+        System.err.println("answer : " + answer);
 
     }
 }
