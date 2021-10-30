99b73581fff272bb99536067f68ff9020c363e23
xio810 <boi4806@naver.com>
2021-10-30 17:15:38 +0900

    복합 대입 연산자

--- src/App.java
+++ src/App.java
@@ -2,19 +2,21 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        int result = 100;
 
-        for (int i = 1; i <= n; i++) {
-            if (n == (i * i)) {
-                System.out.println("양의정수 제곱");
-                System.out.println("원 제곱된 숫자" + i);
-                System.out.println("+1 제곱된 숫자 리턴" + (i + 1) * (i + 1));
-                break;
-            } else
-                System.out.println(-1);
+        System.out.println(result += 5);
+        System.out.println(result -= 5);
+        System.out.println(result /= 5);
+        System.out.println(result %= 5);
+        System.out.println(result *= 2);
 
-        }
+        System.out.println(result <<= 2);
+        System.out.println(result |= 2);
+        System.out.println(result &= 2);
+        System.out.println(result >>>= 3);
+        System.out.println(result >>= 2);
 
     }
 }
