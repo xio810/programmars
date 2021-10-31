20f995bdc565abe783d0b62d20ba9a93eda5f76b
xio810 <boi4806@naver.com>
2021-10-31 17:25:31 +0900

    콜라츠 추측

--- src/App.java
+++ src/App.java
@@ -4,19 +4,25 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Scanner sc = new Scanner(System.in);
         // int n = sc.nextInt();
-        int result = 100;
 
-        System.out.println(result += 5);
-        System.out.println(result -= 5);
-        System.out.println(result /= 5);
-        System.out.println(result %= 5);
-        System.out.println(result *= 2);
+        class Solution {
+            public int solution(int num) {
+                long number = num;
+                int answer;
 
-        System.out.println(result <<= 2);
-        System.out.println(result |= 2);
-        System.out.println(result &= 2);
-        System.out.println(result >>>= 3);
-        System.out.println(result >>= 2);
+                for (answer = 0; answer < 500; answer++) {
+                    if (number == 1)
+                        return answer;
+                    else {
+                        if (number % 2 == 0)
+                            number /= 2;
+                        else
+                            number = number * 3 + 1;
+                    }
+                }
+                return -1;
+            }
+        }
 
     }
 }
