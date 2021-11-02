364d89027fa584e544eb6bc4ef6b1ce866e76102
xio810 <boi4806@naver.com>
2021-11-02 21:56:44 +0900

    형변환, 형확인

--- src/App.java
+++ src/App.java
@@ -1,28 +1,12 @@
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
         // Scanner sc = new Scanner(System.in);
         // int n = sc.nextInt();
 
-        class Solution {
-            public int solution(int num) {
-                long number = num;
-                int answer;
-
-                for (answer = 0; answer < 500; answer++) {
-                    if (number == 1)
-                        return answer;
-                    else {
-                        if (number % 2 == 0)
-                            number /= 2;
-                        else
-                            number = number * 3 + 1;
-                    }
-                }
-                return -1;
-            }
-        }
-
+        String a = "20";
+        Integer b = Integer.parseInt(a);
+        System.out.println(b);
+        System.out.println(a.getClass().getName());
+        System.out.println(b.getClass().getName());
     }
 }
