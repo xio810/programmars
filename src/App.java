70d6ec7dad74370b19485fecde278048915d2b0f
xio810 <boi4806@naver.com>
2021-11-04 20:38:38 +0900

    문자열 다루기 기본

--- src/App.java
+++ src/App.java
@@ -2,13 +2,22 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
         // int n = sc.nextInt();
-        String str = sc.nextLine();
 
-        Integer answer = Integer.parseInt(str);
+        class Solution {
+            public boolean solution(String s) {
+                if (s.length() == 4 || s.length() == 6) {
+                    for (int i = 0; i < s.length(); i++) {
+                        char ch = s.charAt(i);
+                        if (ch < '0' || ch > '9')
+                            return false;
+                    }
+                    return true;
+                } else
+                    return false;
+            }
+        }
 
-        System.out.println(answer);
-        System.out.println("answer type : " + answer.getClass().getName());
     }
 }
