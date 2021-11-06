98d2bf8ee2f9e6088c4ae83c4a6337ec65cc9ec5
xio810 <boi4806@naver.com>
2021-11-06 17:01:39 +0900

    행렬의 덧셈

--- src/App.java
+++ src/App.java
@@ -5,26 +5,22 @@ public class App {
         // Scanner sc = new Scanner(System.in);
         // int n = sc.nextInt();
 
-        int[] arr = { 1, 2, 3, 4, };
-        int sum = 0;
-        double avg = 0;
+        // 이차원배열 값넣기
+        int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 } };
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + "/");
-            sum += arr[i];
+        for (int i = 0; i <= arr.length; i++) {
+            for (int j = 0; j <= arr.length; j++) {
+                System.out.print(arr[i][j] + "/");
+            }
         }
-        avg = sum / arr.length;
+        System.err.println("");
+        System.out.println("arr : " + arr[1][2]);
+        System.out.println("arr.length : " + arr.length);
 
-        System.out.println("sum : " + sum);
-        System.out.println("avg : " + avg);
+        // 선언
+        int[][] arr2 = new int[5][5];
 
-        /*
-         * class Solution { public double solution(int[] arr) { double answer = 0;
-         * double sum = 0;
-         * 
-         * for (int i = 0; i < arr.length; i++) { sum += arr[i]; } answer = sum /
-         * arr.length; return answer; } }
-         */
-
+        arr2[0][1] = 3;
+        System.out.println(arr2[0][1]);
     }
 }
