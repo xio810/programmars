8d500bd7c4a8408aab22ecda0df8b2a1016c30ca
xio810 <boi4806@naver.com>
2021-11-07 17:13:38 +0900

    핸드폰 번호 가리기

--- src/App.java
+++ src/App.java
@@ -5,22 +5,18 @@ public class App {
         // Scanner sc = new Scanner(System.in);
         // int n = sc.nextInt();
 
-        // 이차원배열 값넣기
-        int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 } };
-
-        for (int i = 0; i <= arr.length; i++) {
-            for (int j = 0; j <= arr.length; j++) {
-                System.out.print(arr[i][j] + "/");
+        class Solution {
+            public String solution(String phone_number) {
+                String answer = "";
+                for (int i = 0; i < phone_number.length(); i++) {
+                    if (i < phone_number.length() - 4) {
+                        answer += "*";
+                    } else {
+                        answer += phone_number.charAt(i);
+                    }
+                }
+                return answer;
             }
         }
-        System.err.println("");
-        System.out.println("arr : " + arr[1][2]);
-        System.out.println("arr.length : " + arr.length);
-
-        // 선언
-        int[][] arr2 = new int[5][5];
-
-        arr2[0][1] = 3;
-        System.out.println(arr2[0][1]);
     }
 }
