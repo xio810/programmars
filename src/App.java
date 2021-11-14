ad1e7af499aed5ce09cbd6ffade38384282f754c
xio810 <boi4806@naver.com>
2021-11-14 17:08:08 +0900

    클래스 2

--- src/App.java
+++ src/App.java
@@ -10,6 +10,25 @@ public class App {
         p1.name = "가나다";
 
         p1.introduce();
+
+        //
+
+        Car c1 = new Car();
+        c1.speed = 100;
+
+        Car c2 = new Car();
+        c2.speed = 300;
+
+        c1.run();
+        c2.run();
     }
 
+}
+
+class Car {
+    int speed;
+
+    void run() {
+        System.out.println(speed + "km로 자동차가 달립니다.");
+    }
 }
