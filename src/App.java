14230ce94153b232802e206fb46d9a987f57a852
xio810 <boi4806@naver.com>
2021-11-15 21:12:32 +0900

    클래스 3

--- src/App.java
+++ src/App.java
@@ -6,29 +6,33 @@ public class App {
         // int n = sc.nextInt();
 
         Person p1 = new Person();
-        p1.age = 10;
-        p1.name = "가나다";
+        p1.name = "홍길동";
+        p1.age = 27;
 
-        p1.introduce();
+        Person p2 = new Person();
+        p2.name = "홍길순";
+        p2.age = 25;
 
-        //
+        p1.introduce(); // 안녕하세요 27살 홍길동입니다.
+        p2.introduce(); // 안녕하세요 25살 홍길순입니다.
 
-        Car c1 = new Car();
-        c1.speed = 100;
+        Student s1 = new Student();
+        s1.name = "이순신";
+        s1.math = 90;
+        s1.korean = 87;
+        s1.english = 77;
 
-        Car c2 = new Car();
-        c2.speed = 300;
+        s1.sum(); // 이순신의 총점은 254
+        s1.avg(); // 이순신의 평균은 84.6
 
-        c1.run();
-        c2.run();
+        Student s2 = new Student();
+        s2.name = "임꺽정";
+        s2.math = 70;
+        s2.korean = 97;
+        s2.english = 75;
+
+        s2.sum(); // 임꺽정의 총점은 242
+        s2.avg();
     }
 
-}
-
-class Car {
-    int speed;
-
-    void run() {
-        System.out.println(speed + "km로 자동차가 달립니다.");
-    }
 }
