1b4cc4102b0032a82714d0e277e07ded05b6726c
xio810 <boi4806@naver.com>
2021-11-18 21:31:16 +0900

    클래스 & 배열

--- src/App.java
+++ src/App.java
@@ -4,45 +4,21 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Scanner sc = new Scanner(System.in);
         // int n = sc.nextInt();
+        사람[] 사람들 = new 사람[3];
 
-        VendingMachine vm = new VendingMachine();
+        사람들[0] = new 사람();
+        사람들[1] = new 사람();
+        사람들[2] = new 사람();
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("돈을 투입해주세요 : ");
-        int inputMoney = sc.nextInt();
-        vm.setInputedMoney(inputMoney);
-        // 출력 : 현재 투입된 금액은 2000원입니다.
-
-        System.out.println("음료수를 선택해주세요 : ");
-        vm.printAllBeverages();
-        // 출력 : 0.콜라 : 1000
-        // 출력 : 1.사이다 : 1200
-        // 출력 : 2.커피 : 800
-
-        int no = sc.nextInt();
-        String rst = vm.getBeverage(no);
-        // 출력 : 사이다를 뽑으셨습니다.
-        // 출력 : 잔액은 800원입니다.
-
-        System.out.println(rst);
-        // 출력 : 사이다
-
-        // String rst2 = vm.getBeverage(0);
-        // // 출력 : 잔액이 부족합니다.
-
-        // System.out.println(rst2);
-        // // 출력 : null
-
-        // vm.setInputedMoney(500);
-        // // 출력 : 현재 투입된 금액은 1300원입니다.
-
-        // String rst3 = vm.getBeverage(0);
-        // // 출력 : 콜라를 뽑으셨습니다.
-        // // 출력 : 잔액은 300원입니다.
-        // System.out.println(rst3);
-        // // 출력 : 콜라
-
+        for (int i = 0; i < 사람들.length; i++) {
+            사람들[i].Introduce();
+        }
     }
 
+}
+
+class 사람 {
+    void Introduce() {
+        System.out.println("안녕하세요.");
+    }
 }
--- src/Beverage.java
+++
@@ -1,9 +0,0 @@
-public class Beverage {
-    String name;
-    int price;
-
-    public Beverage(int a, String b) {
-        price = a;
-        name = b;
-    }
-}
