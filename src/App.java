723945a8009ffae004b27c5aa9409820217b3c60
xio810 <boi4806@naver.com>
2021-11-21 05:14:01 +0900

    상속을 통한 캐스팅 허용 2

--- src/App.java
+++ src/App.java
@@ -4,13 +4,15 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Scanner sc = new Scanner(System.in);
         // int n = sc.nextInt();
-        무기 a무기 = new 칼();
-        무기 b무기 = new 무기();
+        칼 a칼 = new 칼();
+        a칼 = new 활();
     }
 }
 
-class 무기 {
+class 칼 {
+
 }
 
-class 칼 extends 무기 {
+class 활 extends 칼 {
+
 }
