c5f5f3807531ecd15f56bfa92bbe899030e8947c
xio810 <boi4806@naver.com>
2021-11-21 05:19:21 +0900

    무기 클래스로 만든 리모콘으로 객체 조종

--- src/App.java
+++ src/App.java
@@ -4,15 +4,28 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Scanner sc = new Scanner(System.in);
         // int n = sc.nextInt();
-        칼 a칼 = new 칼();
-        a칼 = new 활();
+        무기 a무기 = new 칼();
+        a무기.공격();
+        무기 b무기 = new 활();
+        b무기.공격();
     }
 }
 
-class 칼 {
+class 무기 {
+    void 공격() {
+
+    }
+}
+
+class 칼 extends 무기 {
+    void 공격() {
+        System.out.println("칼로 공격합니다.");
+    }
 
 }
 
-class 활 extends 칼 {
-
+class 활 extends 무기 {
+    void 공격() {
+        System.out.println("활로 공격합니다.");
+    }
 }
