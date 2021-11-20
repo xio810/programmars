be94a0ab9cc0258f3bfb428800fd29936f0d2bfc
xio810 <boi4806@naver.com>
2021-11-21 05:41:00 +0900

    매개변수를 사용해서 전사가 매번 다르게 공격하도록

--- src/App.java
+++ src/App.java
@@ -4,28 +4,24 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Scanner sc = new Scanner(System.in);
         // int n = sc.nextInt();
-        무기 a무기 = new 칼();
-        a무기.공격();
-        무기 b무기 = new 활();
-        b무기.공격();
+        전사 a전사 = new 전사();
+
+        a전사.공격("브라이언", "칼");
+        /* 수정가능지역 시작 */ /* 수정가능지역 끝 */
+        // 브라이언이(가) 칼(으)로 공격합니다.
+
+        a전사.공격("필립", "창");
+        // (/* 수정가능지역 시작 */ /* 수정가능지역 끝 */);
+        // // 필립이(가) 창(으)로 공격합니다.
+
+        a전사.공격("마크", "지팡이");
+        // (/* 수정가능지역 시작 */ /* 수정가능지역 끝 */);
+        // 마크(가) 지팡이(으)로 공격합니다.
     }
 }
 
-class 무기 {
-    void 공격() {
-
-    }
-}
-
-class 칼 extends 무기 {
-    void 공격() {
-        System.out.println("칼로 공격합니다.");
-    }
-
-}
-
-class 활 extends 무기 {
-    void 공격() {
-        System.out.println("활로 공격합니다.");
+class 전사 {
+    void 공격(String name, String weapon) {
+        System.out.println(name + "이(가) " + weapon + "(으)로 공격합니다.");
     }
 }
