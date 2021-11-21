932c01f84cdbb9e498d8d732f7a4d5874e6aadcd
xio810 <boi4806@naver.com>
2021-11-22 04:58:00 +0900

    서로 다른 3종 TV를 리모콘 1개로 컨트롤

--- src/App.java
+++ src/App.java
@@ -1,43 +1,95 @@
 import java.util.Scanner;
 
+import javax.sql.rowset.spi.SyncResolver;
+
 public class App {
     public static void main(String[] args) throws Exception {
         // Scanner sc = new Scanner(System.in);
         // int n = sc.nextInt();
-        전사 a전사 = new 전사();
+        샤오미Tv a샤오미Tv = new 샤오미Tv();
+        삼성Tv a삼성Tv = new 삼성Tv();
+        LGTv aLGTv = new LGTv();
 
-        a전사.공격("브라이언", "칼");
-        // 브라이언이(가) 칼(으)로 공격합니다.
+        System.out.println("== 표준Tv 리모콘 들여온 후 ==");
 
-        a전사.재공격();
-        // // 브라이언이(가) 칼(으)로 공격합니다.
+        // 표준Tv 리모콘을 저장할 수 있는 변수를 만든다.
+        표준Tv a표준Tv;
 
-        a전사.공격("필립", "창");
-        // 필립이(가) 창(으)로 공격합니다.
+        // a표준Tv 변수에 샤오미Tv 객체를 연결한다.
+        // 중요 : 다른말로 a샤오미Tv 변수안에 있던 샤오미Tv 리코몬을 표준Tv 리모콘화 해서 a표준Tv변수에 넣는다.
+        a표준Tv = a샤오미Tv;
+        a표준Tv.켜기();
+        // 출력 : 샤오기TV 켜집니다.
+        a표준Tv.끄기();
+        // 출력 : 샤오기TV 꺼집니다.
 
-        a전사.공격("마크", "지팡이");
-        // 마크(가) 지팡이(으)로 공격합니다.
+        // a표준Tv 변수에 삼성Tv 객체를 연결한다.
+        a표준Tv = a삼성Tv;
+        a표준Tv.켜기();
+        // 출력 : 삼성TV 켜집니다.
+        a표준Tv.끄기();
+        // 출력 : 삼성TV 꺼집니다.
 
-        a전사.재공격();
-        // 마크(가) 지팡이(으)로 공격합니다.
+        // a표준Tv 변수에 LGTv 객체를 연결한다.
+        a표준Tv = aLGTv;
+        a표준Tv.켜기();
+        // 출력 : LGTV 켜집니다.
+        a표준Tv.끄기();
+        // 출력 : LGTV 꺼집니다.
 
-        a전사.재공격();
-        // // 마크(가) 지팡이(으)로 공격합니다.
+        // LGTV만의 고유 기능을 표준Tv 리모콘을 이용해서 호출하기 => 불가능
+        // (LGTv) => a표준Tv 변수에 있던 표준Tv 리모콘이 LGTv리모콘화 해서 `aLGTv2` 변수에 들어간다.
+        LGTv aLGTv2 = (LGTv) a표준Tv;
+        aLGTv2.게임모드전환();
     }
 }
 
-class 전사 {
-    String name;
-    String weapon;
-
-    void 공격(String name, String weapon) {
-        this.name = name;
-        this.weapon = weapon;
-
-        재공격();
+class 표준Tv {
+    void 켜기() {
     }
 
-    void 재공격() {
-        System.out.printf("%s(이)가 %s(으)로 공격합니다!\n", name, weapon);
+    void 끄기() {
+    }
+}
+
+class 샤오미Tv extends 표준Tv {
+    void 켜기() {
+        System.out.println("샤오미Tv 켜집니다.");
+    }
+
+    void 끄기() {
+        System.out.println("샤오미Tv 꺼집니다.");
+    }
+
+    void vr켜기() {
+        System.out.println("샤오미Tv vr켜기!");
+    }
+}
+
+class 삼성Tv extends 표준Tv {
+    void 켜기() {
+        System.out.println("삼성Tv 켜집니다.");
+    }
+
+    void 끄기() {
+        System.out.println("삼성Tv 꺼집니다.");
+    }
+
+    void vr켜기() {
+        System.out.println("삼성Tv vr켜기!");
+    }
+}
+
+class LGTv extends 표준Tv {
+    void 켜기() {
+        System.out.println("LGTv 켜집니다.");
+    }
+
+    void 끄기() {
+        System.out.println("LGTv 꺼집니다.");
+    }
+
+    void 게임모드전환() {
+        System.out.println("LGTv 게임모드전환!");
     }
 }
