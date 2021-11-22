be6d8873fd30dfd63864668489dd5e09d2ea3acb
xio810 <boi4806@naver.com>
2021-11-22 21:25:33 +0900

    사람이 a왼팔 이라는 변수를 가질 수 있게

--- src/App.java
+++ src/App.java
@@ -1,41 +1,22 @@
 import java.util.Scanner;
 
-import javax.sql.rowset.spi.SyncResolver;
-
 public class App {
     public static void main(String[] args) throws Exception {
         // Scanner sc = new Scanner(System.in);
         // int n = sc.nextInt();
+        사람 a사람 = new 사람();
 
-        사람 a홍길동 = new 홍길동();
-        System.out.printf("== 홍길동 정보 ==\n");
-        System.out.printf("이름 : %s\n", a홍길동.이름); // 홍길동
-        System.out.printf("나이 : %s\n", a홍길동.나이); // 22
-        사람 a홍길순 = new 홍길순();
-        System.out.printf("== 홍길순 정보 ==\n");
-        System.out.printf("이름 : %s\n", a홍길순.이름); // 홍길순
-        System.out.printf("나이 : %s\n", a홍길순.나이); // 22
-
+        a사람.나이 = 20;
+        a사람.a왼팔 = new 팔();
     }
 }
 
 class 사람 {
-    String 이름;
+    // 클래스안에 변수를 정의 할 수 있다.
     int 나이;
-
-    사람() {
-        this.나이 = 22;
-    }
+    팔 a왼팔;
 }
 
-class 홍길동 extends 사람 {
-    홍길동() {
-        this.이름 = "홍길동";
-    }
-}
+class 팔 {
 
-class 홍길순 extends 사람 {
-    홍길순() {
-        this.이름 = "홍길순";
-    }
 }
